d6e61240e87dcbff876ed45b13aa9196fab0a298
poovarasan <poosan9@gmail.com>
2021-02-22 13:12:29 +0530

    new channels

--- extras/tamil.js
+++ extras/tamil.js
@@ -1,152 +1,269 @@
 module.exports = [
-    {
-        name: 'AUS TAMIL',
-        logo: 'https://austamil.tv/wp-content/uploads/2020/04/aus-tamil-HD-New-e1586081963171.png',
-        url: 'https://bk7l2pn7dx53-hls-live.5centscdn.com/austamil/fe01ce2a7fbac8fafaed7c982a04e229.sdp/chunks.m3u8',
-        category: 'Local'
-    },
-    {
-        name: 'Rasi TV',
-        logo: 'https://i.imgur.com/i9ct2MX.png',
-        url: 'http://singamcloud.in:1935/rasinetwork/rasitv/playlist.m3u8',
-        category: 'Local'
-    },
-    {
-        name: ' Rasi Movies',
-        logo: 'https://i.imgur.com/vqtHzOL.jpg',
-        url: 'http://singamcloud.in:1935/rasinetwork/rasimovies/playlist.m3u8',
-        category: 'Movies'
-    },
-    {
-        name: 'Rasi Classic',
-        logo: 'https://i.imgur.com/vqtHzOL.jpg',
-        url: 'http://singamcloud.in:1935/rasinetwork/rasiclassic/playlist.m3u8',
-        category: 'Local'
-    },
-    {
-        name: 'Sana TV',
-        logo: 'https://i.imgur.com/zAQSZeC.png',
-        url: 'http://7starcloud.com:1935/sanatv/sanatv/playlist.m3u8',
-        category: 'Local'
-    },
-    {
-        name: 'Tirumalai TV KARUR',
-        logo: 'https://yt3.ggpht.com/ytc/AAUvwngU3LWhh8SErFHPH0yKtfsCt4cYrq-EbNnGjICWLA=s88-c-k-c0x00ffffff-no-rj',
-        url: 'http://media.7starcloud.com:1935/tirumalaitv/stream/playlist.m3u8',
-        category: 'Local'
-    },
-    {
-        name: 'Wathanam TV ',
-        logo: 'https://i.imgur.com/4sOEcez.png',
-        url: 'http://95.216.167.183:5080/LiveApp/streams/443106610169904881506470.m3u8',
-        category: 'Local'
-    },
-    {
-        name: 'WORLD TAMIL TV',
-        logo: 'http://www.worldtamil.tv/img/logo.png',
-        url: 'http://185.105.4.244:1935/wtt/wttv/playlist.m3u8',
-        category: 'Local'
-    },
-    {
-        name: 'HARI TV',
-        logo: 'http://haritv.com/images/logo.png',
-        url: 'http://media.7starcloud.com:1935/harimedia/haritv/playlist.m3u8',
-        category: 'Local'
-    },
-    {
-        name: 'Sangamam HD',
-        logo: 'http://sangamamtv.com/images/logo%20header%20banner.png',
-        url: 'http://livematrix.in:1935/live/sangamamtv/playlist.m3u8',
-        category: 'Local'
-    },
-    {
-        name: 'SRI KRISHNA TV',
-        logo: 'http://www.erodesrikrishnatv.com/images/logo.png',
-        url: 'http://media.7starcloud.com:1935/erodekrishnatv/livestream/playlist.m3u8',
-        category: 'Local'
-    },
-    {
-        name: 'MEENAKSHI TV',
-        logo: 'https://www.meenakshitv.in/images/logo.png"',
-        url: 'https://stream.meenakshitv.com/LiveApp/streams/737506415855638572426221.m3u8',
-        category: 'Local'
-    },
-    {
-        name: 'Moon TV',
-        logo: 'http://www.moontvlive.com/images/logo.png',
-        url: 'http://player.mslivestream.net/mslive/e10bb900976df9177b9a080314f26f86.sdp/chunks.m3u8',
-        category: 'Local'
-    },
-    {
-        name: 'AASAI TV',
-        logo: 'http://www.aasaitvlive.in/img/aasai_tv.png',
-        url: 'http://osaitv.livebox.co.in/osaihls/live.m3u8',
-        category: 'Local'
-    },
-    {
-        name: 'VANAVIL',
-        logo: 'https://vaanavil.live/wp-content/uploads/2019/10/Vaanavil-Logo-GIF.gif',
-        url: 'https://6n3yope4d9ok-hls-live.5centscdn.com/vaanavil/TV.stream/chunks.m3u8',
-        category: 'Local'
-    },
-    {
-        name: 'VIDIYAL TV',
-        logo: 'https://i.ytimg.com/vi/R-ZPWaVxX8M/hqdefault.jpg',
-        url: 'http://account7.livebox.co.in/vidiyaltvhls/live.m3u8',
-        category: 'Local'
-    },
-    {
-        name: 'SD Media',
-        logo: 'https://i.imgur.com/FigDbPo.png',
-        url: 'http://7starcloud.com:1935/sd/sdmedia/playlist.m3u8',
-        category: 'Local'
-    },
-    {
-        name: 'ISAIARUVI',
-        logo: 'https://j2apps.s.llnwi.net/isa1/pic/0390.IsaiAruvi.in/en/1x1/208x208/test_pic1552384764086.jpg',
-        url: 'https://m-c049-j2apps.s.llnwi.net/hls/0390.IsaiAruvi.in_360p/index.m3u8',
-        category: 'Local'
-    },
-    {
-        name: 'CAPTAIN TV',
-        logo: 'https://j2apps.s.llnwi.net/isa1/pic/0680.CaptainTV.in/en/1x1/208x208/test_pic1535463113447.jpg',
-        url: 'https://m-c29-j2apps.s.llnwi.net/hls/0680.CaptainTV.in_288p/index.m3u8',
-        category: 'Local'
-    },
-    {
-        name: 'VENDHAR TV',
-        logo: 'https://j2apps.s.llnwi.net/isa1/pic/7077.VendarTV.in/en/1x1/208x208/test_pic1596109828563.jpg',
-        url: 'https://m-c06-j2apps.s.llnwi.net/hls/7077.VendarTV.in_288p/index.m3u8',
-        category: 'Local'
-    },
-    {
-        name: 'TUNE 6',
-        logo: 'https://j2apps.s.llnwi.net/isa1/pic/3731.Tunes6.in/en/1x1/208x208/test_pic1552387113014.jpg',
-        url: 'https://m-c18-j2apps.s.llnwi.net/hls/3731.Tunes6.in.m3u8',
-        category: 'Local'
-    },
-    {
-        name: 'News 18 TAMIL',
-        logo: 'https://static.tamil.news18.com/tamil/uploads/2020/03/tamil-1.png',
-        url: 'https://news18tamil-lh.akamaihd.net/i/n18tamil_1@526595/index_5_av-p.m3u8',
-        category: 'Local'
-    },
-    {
-        name: 'Madha TV',
-        logo: 'https://d229kpbsb5jevy.cloudfront.net/tv/150/150/bnw/Madha_TV-black.png',
-        url: 'http://103.109.45.5:1935/madhatv/madhatv.stream_HDp/chunklist_w67072347.m3u8',
-        category: 'Local'
-    },
-    {
-        name: 'Aasirvadham TV',
-        logo: 'http://7startv.in/astv.png',
-        url: 'http://199.168.96.106:1935/blessingtv-tamillive/mystream/chunklist_w365086533.m3u8',
-        category: 'Local'
-    },
-    {
-        name:  'Marutam TV',
-        url : 'http://mntv.livebox.co.in/mntvhls/mntv.m3u8',
-        category: 'Local',
-        logo: 'http://mntv.in/style/images/logo-dark.png'
-    }
+  {
+    url: 'https://ca.streamjo.com/live/samugam.m3u8',
+    category: 'Local',
+    "logo": "http://jenohdtv.gq/wp-content/uploads/2020/09/Samugam-TV.png",
+    name: "Samugam TV"
+  },
+  {
+    url: 'http://xlbor33arvaj-hls-live.wmncdn.net/peppers/live.stream/index.m3u8',
+    category: 'Local',
+    "logo": "http://jenohdtv.gq/wp-content/uploads/2019/11/Pappers-TV.png",
+    name: "Peppers TV"
+  },
+
+
+  {
+    url: 'http://7starcloud.com:1935/madhimugamtv/madhimugamtv/playlist.m3u8',
+    category: 'Local',
+    "logo": "http://jenohdtv.gq/wp-content/uploads/2019/11/Madhimugam-TV.png",
+    name: "Madhimugam TV"
+  },
+  {
+    url: 'http://103.199.161.254/Content/kalaignarchittiram/Live/Channel(KalaignarChittiram)/Stream(01)/index.m3u8',
+    category: 'Local',
+    "logo": "http://jenohdtv.gq/wp-content/uploads/2019/11/Chithiram-TV.png",
+    name: "Chithiram TV"
+  },
+  {
+    url: 'http://103.199.160.85/Content/kalaignarmurasu/Live/Channel(KalaignarMurasu)/index.m3u8',
+    category: 'Local',
+    "logo": "http://jenohdtv.gq/wp-content/uploads/2019/11/Murasu-TV.png",
+    name: "Murasu TV"
+  },
+  {
+    url: 'http://103.199.161.254/Content/kalaignarsirippoli/Live/Channel(Kalaignarsirippoli)/index.m3u8',
+    category: 'Local',
+    "logo": "http://jenohdtv.gq/wp-content/uploads/2019/11/Srippoli-TV.png",
+    name: "Sirippoli TV"
+  },
+  {
+    url: 'http://vasanth.live-s.cdn.bitgravity.com/cdn-live/_definst_/vasanth/secure/live/feed01/playlist.m3u8?e=0&h=54e44f64b2e6d5122acdf77d01ab7f9f',
+    category: 'Local',
+    "logo": "http://jenohdtv.gq/wp-content/uploads/2019/11/Vasanth-TV.png",
+    name: "Vasanth TV"
+  },
+  {
+    url: 'https://5k8q87azdy4v-hls-live.wmncdn.net/MAKKAL/271ddf829afeece44d8732757fba1a66.sdp/mono.m3u8',
+    category: 'Local',
+    "logo": "http://jenohdtv.gq/wp-content/uploads/2019/11/Makkal-TV.png",
+    name: "Makkal TV"
+  },
+
+
+  {
+    url: 'http://versewsa.pc.cdn.bitgravity.com:80/versewsa/live//kalaignar_tv.smil/chunklist.m3u8',
+    category: 'Local',
+    "logo": "http://jenohdtv.gq/wp-content/uploads/2019/11/Kalainger-Seithigal.png",
+    name: "Kalaignar Seithigal"
+  },
+
+  {
+    url: 'http://versewsa.pc.cdn.bitgravity.com:80/versewsa/live/puthiyathalaimurai.smil/chunklist.m3u8',
+    category: 'Local',
+    "logo": "http://jenohdtv.gq/wp-content/uploads/2019/11/Puthiya-Thalaimuri.png",
+    name: "Puthiyathalaimurai TV"
+  },
+
+  {
+    url: 'http://7starcloud.com:1935/aarya/aaryatv/playlist.m3u8',
+    category: 'Local',
+    "logo": "http://jenohdtv.gq/wp-content/uploads/2019/11/Aaryaa-TV.png",
+    name: "Aaryaa TV"
+  },
+  {
+    url: 'http://streamingbox.7starcloud.com:1935/jawahartv/streamingbox/playlist.m3u8',
+    category: 'Local',
+    name: "Jawahar TV",
+    logo: 'http://jenohdtv.gq/wp-content/uploads/2019/11/Jawahar-Channel.png'
+
+  },
+
+  {
+    url: 'http://mntv.livebox.co.in/mntvhls/mntv.m3u8',
+    category: 'Local',
+    name: "MN TV",
+    logo: 'http://jenohdtv.gq/wp-content/uploads/2019/11/Marutam-Nelli-TV.png'
+
+  },
+  {
+    url: 'http://account12.livebox.co.in/ntvhls/ntv.m3u8',
+    category: 'Local',
+    name: "Raagam HD",
+    logo: 'http://jenohdtv.gq/wp-content/uploads/2019/11/Ragam-TV.png'
+
+  },
+  {
+    url: 'http://player.yarlsriserver.com/ststamiltv/3a311ff8b8f78d17f31045bb51219543.sdp/playlist.m3u8',
+    category: 'Local',
+    name: "STS Tamil TV",
+    logo: 'http://jenohdtv.gq/wp-content/uploads/2019/11/STS-Tamil-TV.png'
+
+  },
+  {
+    name: 'AUS TAMIL',
+    logo: 'https://austamil.tv/wp-content/uploads/2020/04/aus-tamil-HD-New-e1586081963171.png',
+    url: 'https://bk7l2pn7dx53-hls-live.5centscdn.com/austamil/fe01ce2a7fbac8fafaed7c982a04e229.sdp/chunks.m3u8',
+    category: 'Local'
+  },
+  {
+    name: 'Rasi TV',
+    logo: 'https://i.imgur.com/i9ct2MX.png',
+    url: 'http://singamcloud.in:1935/rasinetwork/rasitv/playlist.m3u8',
+    category: 'Local'
+  },
+  {
+    name: 'Rasi Movies',
+    logo: 'https://i.imgur.com/vqtHzOL.jpg',
+    url: 'http://singamcloud.in:1935/rasinetwork/rasimovies/playlist.m3u8',
+    category: 'Movies'
+  },
+  {
+    name: 'Rasi Classic',
+    logo: 'https://i.imgur.com/vqtHzOL.jpg',
+    url: 'http://singamcloud.in:1935/rasinetwork/rasiclassic/playlist.m3u8',
+    category: 'Local'
+  },
+  {
+    name: 'Sana TV',
+    logo: 'https://i.imgur.com/zAQSZeC.png',
+    url: 'http://7starcloud.com:1935/sanatv/sanatv/playlist.m3u8',
+    category: 'Local'
+  },
+  {
+    name: 'Tirumalai TV KARUR',
+    logo: 'https://yt3.ggpht.com/ytc/AAUvwngU3LWhh8SErFHPH0yKtfsCt4cYrq-EbNnGjICWLA=s88-c-k-c0x00ffffff-no-rj',
+    url: 'http://media.7starcloud.com:1935/tirumalaitv/stream/playlist.m3u8',
+    category: 'Local'
+  },
+  {
+    name: 'Wathanam TV ',
+    logo: 'https://i.imgur.com/4sOEcez.png',
+    url: 'http://95.216.167.183:5080/LiveApp/streams/443106610169904881506470.m3u8',
+    category: 'Local'
+  },
+  {
+    name: 'WORLD TAMIL TV',
+    logo: 'http://www.worldtamil.tv/img/logo.png',
+    url: 'http://185.105.4.244:1935/wtt/wttv/playlist.m3u8',
+    category: 'Local'
+  },
+  {
+    name: 'HARI TV',
+    logo: 'http://haritv.com/images/logo.png',
+    url: 'http://media.7starcloud.com:1935/harimedia/haritv/playlist.m3u8',
+    category: 'Local'
+  },
+  {
+    name: 'Sangamam HD',
+    logo: 'http://sangamamtv.com/images/logo%20header%20banner.png',
+    url: 'http://livematrix.in:1935/live/sangamamtv/playlist.m3u8',
+    category: 'Local'
+  },
+  {
+    name: 'MTV',
+    logo: 'http://jenohdtv.gq/wp-content/uploads/2019/11/Tamil-MTV.png',
+    url: 'https://player.yarlsriserver.com/tamilmtv/b9869bec82b6ab24d914fdba43450a1e.sdp/playlist.m3u8',
+    category: 'Local'
+  },
+
+  {
+    name: 'SRI KRISHNA TV',
+    logo: 'http://www.erodesrikrishnatv.com/images/logo.png',
+    url: 'http://media.7starcloud.com:1935/erodekrishnatv/livestream/playlist.m3u8',
+    category: 'Local'
+  },
+  {
+    name: 'MEENAKSHI TV',
+    logo: 'https://www.meenakshitv.in/images/logo.png"',
+    url: 'https://stream.meenakshitv.com/LiveApp/streams/737506415855638572426221.m3u8',
+    category: 'Local'
+  },
+  {
+    name: 'Moon TV',
+    logo: 'http://www.moontvlive.com/images/logo.png',
+    url: 'http://player.mslivestream.net/mslive/e10bb900976df9177b9a080314f26f86.sdp/chunks.m3u8',
+    category: 'Local'
+  },
+  {
+    name: 'AASAI TV',
+    logo: 'http://www.aasaitvlive.in/img/aasai_tv.png',
+    url: 'http://osaitv.livebox.co.in/osaihls/live.m3u8',
+    category: 'Local'
+  },
+  {
+    name: 'VANAVIL',
+    logo: 'https://vaanavil.live/wp-content/uploads/2019/10/Vaanavil-Logo-GIF.gif',
+    url: 'https://6n3yope4d9ok-hls-live.5centscdn.com/vaanavil/TV.stream/chunks.m3u8',
+    category: 'Local'
+  },
+  {
+    name: 'VIDIYAL TV',
+    logo: 'https://i.ytimg.com/vi/R-ZPWaVxX8M/hqdefault.jpg',
+    url: 'http://account7.livebox.co.in/vidiyaltvhls/live.m3u8',
+    category: 'Local'
+  },
+  {
+    name: 'SD Media',
+    logo: 'https://i.imgur.com/FigDbPo.png',
+    url: 'http://7starcloud.com:1935/sd/sdmedia/playlist.m3u8',
+    category: 'Local'
+  },
+  {
+    name: 'ISAIARUVI',
+    logo: 'https://j2apps.s.llnwi.net/isa1/pic/0390.IsaiAruvi.in/en/1x1/208x208/test_pic1552384764086.jpg',
+    url: 'https://m-c049-j2apps.s.llnwi.net/hls/0390.IsaiAruvi.in_360p/index.m3u8',
+    category: 'Local'
+  },
+  {
+    name: 'CAPTAIN TV',
+    logo: 'https://j2apps.s.llnwi.net/isa1/pic/0680.CaptainTV.in/en/1x1/208x208/test_pic1535463113447.jpg',
+    url: 'https://m-c29-j2apps.s.llnwi.net/hls/0680.CaptainTV.in_288p/index.m3u8',
+    category: 'Local'
+  },
+  {
+    name: 'VENDHAR TV',
+    logo: 'https://j2apps.s.llnwi.net/isa1/pic/7077.VendarTV.in/en/1x1/208x208/test_pic1596109828563.jpg',
+    url: 'https://m-c06-j2apps.s.llnwi.net/hls/7077.VendarTV.in_288p/index.m3u8',
+    category: 'Local'
+  },
+  {
+    name: 'TUNE 6',
+    logo: 'https://j2apps.s.llnwi.net/isa1/pic/3731.Tunes6.in/en/1x1/208x208/test_pic1552387113014.jpg',
+    url: 'https://m-c18-j2apps.s.llnwi.net/hls/3731.Tunes6.in.m3u8',
+    category: 'Local'
+  },
+  {
+    name: 'News 18 TAMIL',
+    logo: 'https://static.tamil.news18.com/tamil/uploads/2020/03/tamil-1.png',
+    url: 'https://news18tamil-lh.akamaihd.net/i/n18tamil_1@526595/index_5_av-p.m3u8',
+    category: 'Local'
+  },
+  {
+    name: 'Madha TV',
+    logo: 'https://d229kpbsb5jevy.cloudfront.net/tv/150/150/bnw/Madha_TV-black.png',
+    url: 'http://103.109.45.5:1935/madhatv/madhatv.stream_HDp/chunklist_w67072347.m3u8',
+    category: 'Local'
+  },
+  {
+    name: 'Aasirvadham TV',
+    logo: 'http://7startv.in/astv.png',
+    url: 'http://199.168.96.106:1935/blessingtv-tamillive/mystream/chunklist_w365086533.m3u8',
+    category: 'Local'
+  },
+  {
+    name: 'Marutam TV',
+    url: 'http://mntv.livebox.co.in/mntvhls/mntv.m3u8',
+    category: 'Local',
+    logo: 'http://mntv.in/style/images/logo-dark.png'
+  }
 ]
+
+
+// {
+//   "name": "SUN TV",
+//   "logo": "https://upload.wikimedia.org/wikipedia/en/thumb/6/6a/Sun_TV_logo.svg/1200px-Sun_TV_logo.svg.png",
+//   "url": "http://150p2086.securepoint.io:25461/live/jav/jav123/94794.m3u8?token=TREMWUEJGlxBVVZWBFZcUlRQAAVbUFEGCgIJUQRfAAsOV11aAVFeXwZDShFEQUtSVw5vDwcXDwwHAQFXSURFQVNBO1FWQwJEUlNWBAYNARUaQUQPDlAXDwMaGhMMAhIIFAJXCAFUGkpBBB5HUkdXVlo8VAMVXFZQEQxWFgkKHBBZXTtcVwxZCAdDXBEHFxUVXxBARFlbQFlfGhoBDBJCU0JWRgIQUw9eW0NKEVRaTFlCEUlEWRd8exEaGgYdElVcRVoLVhBbGgtQFF4RGxdQRGkRVRUXR1BUXlNKQV9EABAaEQlZSj5bCQ0PA1BDXFZZRUEKRFEXGRdeWVYKEQlCbUZaABoIQwtfVlZeEUo=",
+//   "category": "Local"
+// },
+
--- tamilradio.js
+++ tamilradio.js
@@ -25,10 +25,141 @@ fs.readFile(path.join(__dirname, 'extras', 'tamil.txt'), 'utf8', function (err, 
 
   const aData = {
     showLogo: true,
+    language: "Tamil",
     radio: fullChannel
   }
 
-  fs.writeFile(path.join(__dirname , 'api','tamilradio.json'), JSON.stringify(aData) , (a) => {
+  fs.writeFile(path.join(__dirname, 'api', 'tamilradio.json'), JSON.stringify(aData), (a) => {
+    console.log("DONE")
+  })
+
+});
+
+
+fs.readFile(path.join(__dirname, 'extras', 'telegu.txt'), 'utf8', function (err, data) {
+  const splittedRadio = data.split(SEPERATOR)
+
+  const fullChannel = [];
+  splittedRadio.forEach((v, i) => {
+    const singleChannel = v.split("\n");
+    const dryed = singleChannel.filter(x => x !== "")
+
+    if (dryed.length === 3) {
+
+      const name = dryed[1].trim()
+
+      fullChannel.push({
+        "url": dryed[0],
+        "name": name.charAt(0).toUpperCase() + name.slice(1),
+        "logo": dryed[2]
+      })
+    }
+  })
+
+  const aData = {
+    showLogo: true,
+    language: "Telegu",
+    radio: fullChannel
+  }
+
+  fs.writeFile(path.join(__dirname, 'api', 'teleguradio.json'), JSON.stringify(aData), (a) => {
+    console.log("DONE")
+  })
+
+});
+
+fs.readFile(path.join(__dirname, 'extras', 'malyalam.txt'), 'utf8', function (err, data) {
+  const splittedRadio = data.split(SEPERATOR)
+
+  const fullChannel = [];
+  splittedRadio.forEach((v, i) => {
+    const singleChannel = v.split("\n");
+    const dryed = singleChannel.filter(x => x !== "")
+
+    if (dryed.length === 3) {
+
+      const name = dryed[1].trim()
+
+      fullChannel.push({
+        "url": dryed[0],
+        "name": name.charAt(0).toUpperCase() + name.slice(1),
+        "logo": dryed[2]
+      })
+    }
+  })
+
+  const aData = {
+    showLogo: true,
+    language: "Malayalam",
+    radio: fullChannel
+  }
+
+  fs.writeFile(path.join(__dirname, 'api', 'malyalamradio.json'), JSON.stringify(aData), (a) => {
+    console.log("DONE")
+  })
+
+});
+
+
+fs.readFile(path.join(__dirname, 'extras', 'kannada.txt'), 'utf8', function (err, data) {
+  const splittedRadio = data.split(SEPERATOR)
+
+  const fullChannel = [];
+  splittedRadio.forEach((v, i) => {
+    const singleChannel = v.split("\n");
+    const dryed = singleChannel.filter(x => x !== "")
+
+    if (dryed.length === 3) {
+
+      const name = dryed[1].trim()
+
+      fullChannel.push({
+        "url": dryed[0],
+        "name": name.charAt(0).toUpperCase() + name.slice(1),
+        "logo": dryed[2]
+      })
+    }
+  })
+
+  const aData = {
+    showLogo: true,
+    language: "Kannada",
+    radio: fullChannel
+  }
+
+  fs.writeFile(path.join(__dirname, 'api', 'kannadaradio.json'), JSON.stringify(aData), (a) => {
+    console.log("DONE")
+  })
+
+});
+
+fs.readFile(path.join(__dirname, 'extras', 'bollywood.txt'), 'utf8', function (err, data) {
+  const splittedRadio = data.split(SEPERATOR)
+
+  const fullChannel = [];
+  splittedRadio.forEach((v, i) => {
+    const singleChannel = v.split("\n");
+    const dryed = singleChannel.filter(x => x !== "")
+
+    if (dryed.length === 3) {
+
+      const name = dryed[1].trim()
+
+      fullChannel.push({
+        "url": dryed[0],
+        "name": name.charAt(0).toUpperCase() + name.slice(1),
+        "logo": dryed[2]
+      })
+    }
+  })
+
+  const aData = {
+    showLogo: true,
+    language: "Bollywood",
+    radio: fullChannel
+  }
+
+  fs.writeFile(path.join(__dirname, 'api', 'bollywoodradio.json'), JSON.stringify(aData), (a) => {
     console.log("DONE")
   })
 
