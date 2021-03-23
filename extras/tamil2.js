df385f9b8ee80833a24636c5249bd4de156d7d37
poovarasan <poosan9@gmail.com>
2021-03-23 16:57:36 +0530

    new added

--- extras/tamil2.js
+++ extras/tamil2.js
@@ -1,3 +1,125 @@
 module.exports = [
+  {
+    name : "Gospel",
+    category: 'Local',
+    url : "rtmp://bmlive.net:1935/gospeltv/gospeltv",
+    logo: "https://raw.githubusercontent.com/poovarasanvasudevan/iptvapi/main/extras/images/2.png"
+  },
+  {
+    name : "Apple TV",
+    category: 'Local',
+    url : "rtmp://bmlive.net:1935/appletv/appletv",
+    logo: "https://raw.githubusercontent.com/poovarasanvasudevan/iptvapi/main/extras/images/3.png"
+  },
+  {
+    name : "Sri TV",
+    category: 'Local',
+    url : "rtmp://bmlive.net:1935/sritv/sritv",
+    logo: "https://raw.githubusercontent.com/poovarasanvasudevan/iptvapi/main/extras/images/7.png"
+  },
+  {
+    name : "Jesus TV",
+    category: 'Religious',
+    url : "rtmp://bmlive.net:1935/jesuschannel/jesuschannel",
+    logo: "https://raw.githubusercontent.com/poovarasanvasudevan/iptvapi/main/extras/images/21.png"
+  },
+  {
+    name : "Digital TV",
+    category: 'Local',
+    url : "rtmp://bmlive.net:1935/digitaltv/digitaltv",
+    logo: "https://raw.githubusercontent.com/poovarasanvasudevan/iptvapi/main/extras/images/22.png"
+  },
+  {
+    name : "CTN TV",
+    category: 'Local',
+    url : "rtmp://stream.blackmoon.in:1935/ctn/ctn",
+    logo: "https://raw.githubusercontent.com/poovarasanvasudevan/iptvapi/main/extras/images/29.png"
+  },
+  {
+    name : "Tirupur TV",
+    category: 'Local',
+    url : "rtmp://bmlive.net:1935/tiruputv/tirupurtv",
+    logo: "https://raw.githubusercontent.com/poovarasanvasudevan/iptvapi/main/extras/images/35.png"
+  },
+  {
+    name : "Kumari TV",
+    category: 'Local',
+    url : "rtmp://bmlive.net:1935/kumaritv/kumaritv",
+    logo: "https://raw.githubusercontent.com/poovarasanvasudevan/iptvapi/main/extras/images/36.png"
+  },
+  {
+    name : "NEEM TV",
+    category: 'Local',
+    url : "rtmp://bmlive.net:1935/NEEMTv/neemlivetv",
+    logo: "https://raw.githubusercontent.com/poovarasanvasudevan/iptvapi/main/extras/images/42.png"
+  },
+  {
+    name : "Arasu TV",
+    category: 'Local',
+    url : "rtmp://bmlive.net:1935/arasutv/arasutv",
+    logo: "https://raw.githubusercontent.com/poovarasanvasudevan/iptvapi/main/extras/images/45.png"
+  },
+  {
+    name : "Amma Channel",
+    category: 'Local',
+    url : "rtmp://bmlive.net:1935/ammachannel/ammachannel",
+    logo: "https://raw.githubusercontent.com/poovarasanvasudevan/iptvapi/main/extras/images/46.png"
+  },
+  {
+    name : "CTN Channel",
+    category: 'Local',
+    url : "rtmp://bmlive.net:1935/ctnchannel/ctnchannel",
+    logo: "https://raw.githubusercontent.com/poovarasanvasudevan/iptvapi/main/extras/images/47.png"
+  },
+  {
+    name : "KKTDC HD",
+    category: 'Local',
+    url : "rtmp://bmlive.net:1935/kktdctv/kktdctv",
+    logo: "https://raw.githubusercontent.com/poovarasanvasudevan/iptvapi/main/extras/images/55.png"
+  },
+  {
+    name : "Sky HD",
+    category: 'Local',
+    url : "rtmp://bmlive.net:1935/dharmatv/dharmatv",
+    logo: "https://raw.githubusercontent.com/poovarasanvasudevan/iptvapi/main/extras/images/57.png"
+  },
+  {
+    name : "Thalapathy TV",
+    category: 'Local',
+    url : "rtmp://bmlive.net:1935/Thalapathyvj/thalapathy",
+    logo: "https://raw.githubusercontent.com/poovarasanvasudevan/iptvapi/main/extras/images/70.png"
+  },
+  {
+    name : "Jeyam TV",
+    category: 'Local',
+    url : "rtmp://bmlive.net:1935/jeyamtvhd/JeyamTv",
+    logo: "https://raw.githubusercontent.com/poovarasanvasudevan/iptvapi/main/extras/images/75.png"
+  },
+  {
+    name : "Wins TV",
+    category: 'Local',
+    url : "rtmp://bmlive.net:1935/WINS/winstv",
+    logo: "https://raw.githubusercontent.com/poovarasanvasudevan/iptvapi/main/extras/images/76.png"
+  },
+  {
+    name : "SR TV",
+    category: 'Local',
+    url : "rtmp://210.18.155.135:1935/live/srtv",
+    logo: "https://raw.githubusercontent.com/poovarasanvasudevan/iptvapi/main/extras/images/83.png"
+  },
+  {
+    name : "Zara TV",
+    category: 'Local',
+    url : "rtmp://bmlive.net:1935/zaratv/zaratv",
+    logo: "https://raw.githubusercontent.com/poovarasanvasudevan/iptvapi/main/extras/images/87.png"
+  },
+  {
+    name : "Ashoka TV",
+    category: 'Local',
+    url : "rtmp://bmlive.net:1935/asokatv/asokatvv",
+    logo: "https://raw.githubusercontent.com/poovarasanvasudevan/iptvapi/main/extras/images/102.png"
+  }
+
+
 
 ]
