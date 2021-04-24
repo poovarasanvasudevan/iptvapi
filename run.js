b174317c0d00bd1a9ea77a7a95a0b9b7386f47e5
poovarasan <poosan9@gmail.com>
2021-04-24 15:17:02 +0530

    arabic tv added

--- run.js
+++ run.js
@@ -245,5 +245,37 @@ function createData(json) {
     })
   });
 
+
+  let rawdata = fs.readFileSync(path.join(__dirname, 'api', 'language', 'ara.json'));
+  let arabiclanguage = JSON.parse(rawdata);
+
+  var arabiccat = {};
+  arabiclanguage.forEach(d => {
+    if (d.category === null) {
+      if (arabiccat.hasOwnProperty("Other")) {
+        arabiccat['Other'].push(d)
+      } else {
+        arabiccat['Other'] = []
+        arabiccat['Other'].push(d)
+      }
+    } else {
+      if (arabiccat.hasOwnProperty(d.category)) {
+        arabiccat[d.category].push(d)
+      } else {
+        arabiccat[d.category] = []
+        arabiccat[d.category].push(d)
+      }
+    }
+  })
+
+
+  var b = []
+  Object.keys(arabiccat).forEach(k => {
+    fs.writeFileSync(path.join(__dirname, 'api', 'arabictv', `${k}.json`), JSON.stringify(arabiccat[k]));
+    b.push({"file": `${k}.json`, 'title': k})
+  })
+
+  fs.writeFileSync(path.join(__dirname, 'api', 'arabictv', 'index.json'), JSON.stringify(b));
+  fs.writeFileSync(path.join(__dirname, 'api', 'arabictv', 'full.json'), JSON.stringify(arabiclanguage));
   console.log("end all")
 }
