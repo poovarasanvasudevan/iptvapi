07eb699ff26fabf31dca9fe2e4af64f2b5665563
poovarasan <poosan9@gmail.com>
2021-05-08 23:21:20 +0530

    new channel

--- run.js
+++ run.js
@@ -246,38 +246,38 @@ function createData(json) {
   });
 
 
-  let rawdata = fs.readFileSync(path.join(__dirname, 'api', 'language', 'ara.json'));
-  let arabiclanguage = JSON.parse(rawdata);
-
-  var arabiccat = {};
-  arabiclanguage.forEach(d => {
-    if (d.category === null) {
-      if (arabiccat.hasOwnProperty("Other")) {
-        arabiccat['Other'].push(d)
-      } else {
-        arabiccat['Other'] = []
-        arabiccat['Other'].push(d)
-      }
-    } else {
-      if (arabiccat.hasOwnProperty(d.category)) {
-        arabiccat[d.category].push(d)
-      } else {
-        arabiccat[d.category] = []
-        arabiccat[d.category].push(d)
-      }
-    }
-  })
-
-
-  var b = []
-  Object.keys(arabiccat).forEach(k => {
-    fs.writeFileSync(path.join(__dirname, 'api', 'arabictv', `${k}.json`), JSON.stringify(arabiccat[k]));
-    b.push({"file": `${k}.json`, 'title': k})
-  })
-
-  fs.writeFileSync(path.join(__dirname, 'api', 'arabictv', 'index.json'), JSON.stringify(b));
-  fs.writeFileSync(path.join(__dirname, 'api', 'arabictv', 'full.json'), JSON.stringify(arabiclanguage));
-
+  // let rawdata = fs.readFileSync(path.join(__dirname, 'api', 'language', 'ara.json'));
+  // let arabiclanguage = JSON.parse(rawdata);
+  //
+  // var arabiccat = {};
+  // arabiclanguage.forEach(d => {
+  //   if (d.category === null) {
+  //     if (arabiccat.hasOwnProperty("Other")) {
+  //       arabiccat['Other'].push(d)
+  //     } else {
+  //       arabiccat['Other'] = []
+  //       arabiccat['Other'].push(d)
+  //     }
+  //   } else {
+  //     if (arabiccat.hasOwnProperty(d.category)) {
+  //       arabiccat[d.category].push(d)
+  //     } else {
+  //       arabiccat[d.category] = []
+  //       arabiccat[d.category].push(d)
+  //     }
+  //   }
+  // })
+  //
+  //
+  // var b = []
+  // Object.keys(arabiccat).forEach(k => {
+  //   fs.writeFileSync(path.join(__dirname, 'api', 'arabictv', `${k}.json`), JSON.stringify(arabiccat[k]));
+  //   b.push({"file": `${k}.json`, 'title': k})
+  // })
+  //
+  // fs.writeFileSync(path.join(__dirname, 'api', 'arabictv', 'index.json'), JSON.stringify(b));
+  // fs.writeFileSync(path.join(__dirname, 'api', 'arabictv', 'full.json'), JSON.stringify(arabiclanguage));
+  //
 
   let russianData = fs.readFileSync(path.join(__dirname, 'api', 'country', 'ru.json'));
   let russianLanguage = JSON.parse(russianData);
